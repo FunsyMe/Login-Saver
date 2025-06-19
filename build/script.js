5050f3e64d55385614e7bf146a55745149534322
Funsy <egorabramovich2012@mail.ru>
2025-06-19 12:23:25 +0300

    Create script.js

---
+++ build/script.js
@@ -0,0 +1,9 @@
+function setInfo() {
+    document.getElementById("extra-container").innerHTML = 
+    '<p>Login-Saver - плагин для безопасности сервера Minecraft. С помощью него, мы предлагаем защитить Ваш сервер от взломов аккаунтов игроков и много другого</p>';
+}
+
+function setUsage() {
+    document.getElementById("extra-container").innerHTML =
+    '<p>Чтобы зарегистрироваться - введи команду в чат /register <пароль> <повтор_пароля> <телеграм_айди>. Чтобы залогиниться - введите команду /login <пароль>. Подробную информацию ищите <a href="https://example.com/" class="link">здесь</a></p>';
+}
